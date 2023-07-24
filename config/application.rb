bf44d77d3368b3cd44d4fc0d4063130731036c19
kohigh <kohigh048@gmail.com>
2023-07-24 11:29:33 +0200

    Remove redundant tools(active_storage/engine, action_view/railtie, action_mailer/railtie, active_job/railtie, action_cable/engine, action_mailbox/engine, action_text/engine) and its dependencies.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,17 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+
+%w(
+  active_record/railtie
+  action_controller/railtie
+  rails/test_unit/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
